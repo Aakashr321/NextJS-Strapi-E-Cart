9019c3cab2cc1129bca83802e989c6b058bdf912
Aakash Rathore <aakash.rathore321@gmail.com>
2022-11-02 12:25:16 +0530

    created products list page

--- frontend/pages/products.js
+++ frontend/pages/products.js
@@ -1,11 +1,12 @@
+import Link from "next/link";
 import React from "react";
 
 const Products = (props) => {
   return (
     <div className="container mx-auto px-4">
       <section className="text-gray-600 body-font">
-        <div className="container px-5 py-24 mx-auto">
-          <div className="flex flex-wrap w-full mb-20">
+        <div className="container px-5 md:py-24 mx-auto">
+          <div className="flex flex-wrap w-full md:mb-20">
             <div className="lg:w-1/2 w-full mb-6 lg:mb-0">
               <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">
                 Product List - MyShop
@@ -19,7 +20,7 @@ const Products = (props) => {
                 <div key={item.id} className="xl:w-1/4 md:w-1/2 p-4">
                   <div className="bg-gray-100 p-6 rounded-lg">
                     <img
-                      className="h-40 rounded w-full object-cover object-center mb-6"
+                      className="h-96 rounded m-auto mb-8"
                       src={`http://localhost:1337${item.attributes.image.data.attributes.formats.thumbnail.url}`}
                       alt="content"
                     />
@@ -31,9 +32,12 @@ const Products = (props) => {
                     </h2>
                     <div className="hidden bg-red-800 bg-purple-800 bg-green-800 bg-black-800 bg-blue-800 bg-yellow-800 bg-white-800"></div>
                     <button className={"border-2 border-gray-300 ml-1 rounded-full w-6 h-6 focus:outline-none " + `bg-${item.attributes.color}-800`}></button>
-                    <p className="leading-relaxed text-base">
-                      {item.attributes.description}
+                    <p className="leading-relaxed text-base" >
+                      {item.attributes.description.substring(0,70)}...
                     </p>
+                    <Link href={`/product/${item.attributes.slug}`}>
+                    <button class="my-2 text-white bg-indigo-500 border-0 py-1 md:py-2 px-2 md:px-4 focus:outline-none hover:bg-indigo-600 rounded text-sm">Buy Now</button>
+                    </Link>
                   </div>
                 </div>
               );
